eb5e7ccedea60275c4ad807ea0140b080247b7ba
dowellbe <36481749+dowellbe@users.noreply.github.com>
2018-04-30 01:44:46 -0400

    First commit

---
+++ js/fundamentals/operator.js
@@ -0,0 +1,18 @@
+var x = 6;		// assigned value of 6 to x
+var y = "6";	// assigned value of 6 to y
+var z = 3;   	// assigned value of 3 to z
+
+// Comparison Operators
+x == y; 	// Equal; true
+x === y; 	// Strict Equal; false
+x != y; 	// Not Equal; false
+x !== y;	// Strict Not Equal; true
+x < z; 		// Less Than; false
+x <= y;		// Less Than or Equal to; true
+x > z;		// Greater Than; true
+x >= y;		// Greater Than or Equal to; true
+
+// Logical Operators
+x > 1 && x < 10; 	// And (&&) Operator; true
+x > 1 || x < 5; 	// Or (||) Operator; true
+!x 					// Not (!) Operator; false
