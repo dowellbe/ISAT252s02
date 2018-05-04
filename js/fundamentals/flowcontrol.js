7fbe1bd571bd82fe1ca0e62b343b665afa2ac384
dowellbe <36481749+dowellbe@users.noreply.github.com>
2018-05-03 22:15:41 -0400

    First commit

---
+++ js/fundamentals/flowcontrol.js
@@ -0,0 +1,14 @@
+var age = 20  
+// The current age is 20.
+if (age < 18) { 
+    text = "Not of legal voting age.";
+// If current age is less than 18, 
+// output is "Not of legal voting age."
+} else if (age < 21) {
+    text = "Not of legal drinking age.";
+// If current age is less than 21, 
+// output is "Not of legal drinking age."
+} else {
+    text = "Too young or too old.";
+}
+// Output is "Not of legal drinking age."
